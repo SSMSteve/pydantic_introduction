7bfe95ad230a025f1d26b13c8cd6e2d5e9e74e41
SSMSteve <steve@Steve-XPS.>
2025-09-15 20:15:21 -0700

    final

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from pydantic import BaseModel,  Field,validator
 import uuid
 from datetime import date, datetime, timedelta
+from enums import Department
 
 class Student(BaseModel):
     id: uuid.UUID
@@ -10,7 +11,7 @@ class Student(BaseModel):
     date_of_birth: date
     GPA: Annotated[float, Field(ge=0, le=4)]
     course: str | None = None
-    department: str
+    department: Department
     fees_paid: bool
     
     @validator("date_of_birth")
@@ -23,9 +24,11 @@ class Student(BaseModel):
 def main():
     with open("students_v1.json") as f:
         data = json.load(f)
-        data.append({"id": "d15782d9-3d8f-4624-a88b-c8e836569df8", "name": "Eric Travis", "date_of_birth": "2010-05-25", "GPA": "4.0", "course": "Computer Science", "department": "Science and Engineering", "fees_paid": False})
+        # data.append({"id": "d15782d9-3d8f-4624-a88b-c8e836569df8", "name": "Eric Travis", "date_of_birth": "2010-05-25", "GPA": "4.0", "course": "Computer Science", "department": "Science and Engineering", "fees_paid": False})
     for student in data:
-        print(Student(**student))
+        my_student = Student(**student)
+        print(my_student.dict())
+        print(my_student.json())
     
 if __name__ == "__main__":
     main()
